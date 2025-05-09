6f61dbb9d58597a427784aa7ab2bcb108a447897
frutillitaaaa <daniela.castro02@alumnos.ucn.cl>
2025-05-09 05:28:31 -0400

    correcciones api usuarios

--- backend/catalogos/src/proveedores/proveedores.controller.ts
+++ backend/catalogos/src/proveedores/proveedores.controller.ts
@@ -8,40 +8,40 @@ import {
   Param,
   ParseIntPipe,
 } from '@nestjs/common';
-import { ProveedorService } from './proveedores.service';
-import { Proveedor } from '@prisma/client'
+import { ProveedoresService } from './proveedores.service';
+import { Proveedor } from '@prisma/client';
 
 @Controller('catalogos/proveedores')
 export class ProveedoresController {
-    constructor(private readonly usuariosService: ProveedoresService) {}
+  constructor(private readonly usuariosService: ProveedoresService) {}
 
-    @Get()
-    async getAll(): Promise<Proveedor[]> {
-        return this.usuariosService.findAll();
-    }
+  @Get()
+  async getAll(): Promise<Proveedor[]> {
+    return this.usuariosService.findAll();
+  }
 
-    @Get(':id')
-    async getById(
-        @Param('id', ParseIntPipe) id: number,
-    ): Promise<Proveedor | null> {
-        return this.usuariosService.findById(id);
-    }
+  @Get(':id')
+  async getById(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<Proveedor | null> {
+    return this.usuariosService.findById(id);
+  }
 
-    @Post()
-    async create(@Body() data: Proveedor): Promise<Proveedor> {
-        return this.usuariosService.create(data);
-    }
+  @Post()
+  async create(@Body() data: Proveedor): Promise<Proveedor> {
+    return this.usuariosService.create(data);
+  }
 
-    @Put(':id')
-    async update(
-        @Param('id', ParseIntPipe) id: number,
-        @Body() data: Partial<Proveedor>,
-    ): Promise<Proveedor> {
-        return this.usuariosService.update(id, data);
-    }
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() data: Partial<Proveedor>,
+  ): Promise<Proveedor> {
+    return this.usuariosService.update(id, data);
+  }
 
-    @Delete(':id')
-    async delete(@Param('id', ParseIntPipe) id: number): Promise<Proveedor> {
-        return this.usuariosService.delete(id);
-    }
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id: number): Promise<Proveedor> {
+    return this.usuariosService.delete(id);
+  }
 }
--- backend/usuarios/src/usuarios/usuarios.module.ts
+++ backend/usuarios/src/usuarios/usuarios.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { UsuariosController } from './usuarios.controller';
+import { UsuariosService } from './usuarios.service';
 
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  controllers: [UsuariosController],
+  providers: [UsuariosService, PrismaService],
 })
-export class PrismaModule {}
+export class UsuariosModule {}
